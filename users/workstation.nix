e35e7a6f4afb7a2979054801b6c78f17dc952912
michael <mjborthwick77@gmail.com>
2026-09-03 22:04:57 +1000

    Configure Vim and Alacritty in workstation.nix
    Added configuration for Vim and Alacritty with specific font settings.

--- users/workstation.nix
+++ users/workstation.nix
@@ -1,1 +1,32 @@
-a
+{ pkgs, ... }: {
+  imports = [ ./michael.nix ];
+
+  programs.vim = {
+    enable = true;
+    extraConfig = ''
+      set number
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        size = 16.0;
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        bold = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Bold";
+        };
+        italic = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Italic";
+        };
+      };
+    };
+  }; 
+
+}
