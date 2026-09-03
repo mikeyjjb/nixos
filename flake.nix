23e0490aa82a49e0b33fc78c31477d7332464ecc
michael <mjborthwick77@gmail.com>
2026-09-03 20:55:31 +1000

    Create flake.nix for NixOS configuration
    Added Nix flake configuration for home multisystem setup with two computers.

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "home multisystem nixos configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+      radar = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./common/default.nix
+          ./computers/radar/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.michael = import ./users/michael.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+      rizzo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./common/default.nix
+          ./cocmputers/rizzo/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.michael = import ./users/michael.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
