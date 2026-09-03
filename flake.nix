b7c89b106ef7f4dedc00ada880aaa725b5a87202
michael <mjborthwick77@gmail.com>
2026-09-04 08:01:05 +1000

    Add nixosConfigurations for radar system

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-
+    nixosConfigurations = {
       radar = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
@@ -43,5 +43,5 @@
         ];
       };
     };
-
+  };
 }
