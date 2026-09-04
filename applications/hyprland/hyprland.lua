31c8f6dfab2cc98fcff88642460a0925654c767d
michael <mjborthwick77@gmail.com>
2026-09-04 20:21:21 +1000

    Remove Steam key binding from hyprland.lua
    Removed the key binding for launching Steam.

--- applications/hyprland/hyprland.lua
+++ applications/hyprland/hyprland.lua
@@ -264,7 +264,6 @@ hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("kitty"))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd("discord"))
 hl.bind(mainMod .. " + F", hl.dsp.exec_cmd("firefox"))
 hl.bind(mainMod .. " + I", hl.dsp.exec_cmd("waytrogen"))
-hl.bind(mainMod .. " + S", hl.dsp.exec_cmd("steam"))
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 --hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
