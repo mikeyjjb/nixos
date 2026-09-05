f0c0bc72f6412bd5405300630113479ca516abc7
michael <mjborthwick77@gmail.com>
2026-09-05 12:26:30 +1000

    Add window rules for eve-online-launcher

--- applications/hyprland/hyprland.lua
+++ applications/hyprland/hyprland.lua
@@ -325,6 +325,8 @@ hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = tr
 
 windowrulev2 = tile, class:^(eve)$ 
 windowrulev2 = forceinput, class:^(eve)$
+windowrulev2 = float, class:^(eve-online-launcher)$ 
+windowrulev2 = center, class:^(eve-online-launcher)$
 
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
