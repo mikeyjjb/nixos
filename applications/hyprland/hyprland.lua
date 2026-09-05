073d6824b6cce5431f90c7d7905b070bdec0ccb2
michael <mjborthwick77@gmail.com>
2026-09-05 15:09:15 +1000

    Update hyprland.lua

--- applications/hyprland/hyprland.lua
+++ applications/hyprland/hyprland.lua
@@ -44,6 +44,7 @@ local menu        = "hyprlauncher"
 hl.on("hyprland.start", function () 
 --   hl.exec_cmd(terminal)
 --   hl.exec_cmd("nm-applet")
+  hl.exec_cmd("hyprpaper")
   hl.exec_cmd("waybar")
 end)
 
