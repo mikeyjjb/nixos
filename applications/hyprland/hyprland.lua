2fad5423b0b564c4a5c2083eadbde6bae1e79196
michael <mjborthwick77@gmail.com>
2026-09-05 16:57:32 +1000

    Update hyprland.lua

--- applications/hyprland/hyprland.lua
+++ applications/hyprland/hyprland.lua
@@ -41,7 +41,8 @@ local menu        = "hyprlauncher"
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function ()
-	hl.exec_cmd("hyprpaper")
+	-- hl.exec_cmd("hyprpaper")
+	hl.exec_cmd("/home/michael/Scripts/wallpaper.zsh")
     hl.exec_cmd("waybar")
 end)
 
