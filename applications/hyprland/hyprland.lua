93cd8b85d182cb2916e9ad3aefe75cabde7d8175
michael <mjborthwick77@gmail.com>
2026-09-05 15:39:40 +1000

    Add hyprpaper to autostart processes

--- applications/hyprland/hyprland.lua
+++ applications/hyprland/hyprland.lua
@@ -40,7 +40,8 @@ local menu        = "hyprlauncher"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
-hl.on("hyprland.start", function () 
+hl.on("hyprland.start", function ()
+	hl.exec_cmd("hyprpaper")
     hl.exec_cmd("waybar")
 end)
 
